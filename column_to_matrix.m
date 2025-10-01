01364257665372ba3ead740ce882f9ff1628ee78
Cianzoid28 <157330096+Cianzoid28@users.noreply.github.com>
2025-10-01 00:34:16 -0400

    fixing link length error func

--- column_to_matrix.m
+++ column_to_matrix.m
@@ -1,4 +1,3 @@
-% The code for f1(V ) = M is provided below:
 %Converts from the column vector form of the coordinates to a
 %friendlier matrix form
 %INPUTS:
@@ -6,6 +5,6 @@
 %OUTPUTS:
 %coords_out = [x1,y1;x2,y2;...;xn,yn] (n x 2 matrix)
 function coords_out = column_to_matrix(coords_in)
-num_coords = length(coords_in);
-coords_out = [coords_in(1:2:(num_coords-1)),coords_in(2:2:num_coords)];
+    num_coords = length(coords_in);
+    coords_out = [coords_in(1:2:(num_coords-1)),coords_in(2:2:num_coords)];
 end
--- matrix_to_column.m
+++ matrix_to_column.m
@@ -6,8 +6,8 @@
 %OUTPUTS:
 %coords_out = [x1;y1;x2;y2;...;xn;yn] (2n x 1 column vector)
 function coords_out = matrix_to_column(coords_in)
-num_coords = 2*size(coords_in,1);
-coords_out = zeros(num_coords,1);
-coords_out(1:2:(num_coords-1)) = coords_in(:,1);
-coords_out(2:2:num_coords) = coords_in(:,2);
+    num_coords = 2*size(coords_in,1);
+    coords_out = zeros(num_coords,1);
+    coords_out(1:2:(num_coords-1)) = coords_in(:,1);
+    coords_out(2:2:num_coords) = coords_in(:,2);
 end
